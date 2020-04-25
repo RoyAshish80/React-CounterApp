e0de222fcbdc52b76f4e641056dbc927ba2d835b
Ashish Roy <bits.ashish@outlook.com>
2020-04-25 22:49:33 +0530

    Implementing single source of truth and removing the local state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,14 +10,37 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id != counterId);
+    this.setState({ counters });
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    console.log(this.state.counters[index]);
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
   render() {
     return (
       <div>
+        <button onClick="this.handleReset" className="btn-primary btn-sm m-2">
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            counter={counter}
           ></Counter>
         ))}
       </div>
